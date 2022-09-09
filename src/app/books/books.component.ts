9095c21afad36575c4a04b1e2319f53faefb1b74
suedarby <sue.a.darby@gmail.com>
2022-09-09 10:22:46 -0800

    button works

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
+name: string = "Outlander";
+author: string = "Dianna Gabaldon";
+src: string = "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51PrW27sXWL.jpg";
+
+isDisabled: boolean = false;
+
+handleClick(){
+  alert('I am working!')
+};
+
   constructor() { }
 
   ngOnInit(): void {
