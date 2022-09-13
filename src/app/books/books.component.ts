c1559dd4ad9971b49810f81cf5c26ca592dc6b1c
suedarby <sue.a.darby@gmail.com>
2022-09-13 10:49:43 -0800

    ngif ng-container

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -7,15 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
+  // SHOWS THE STATIC VARIABLES
+
 name: string = "Outlander";
 author: string = "Dianna Gabaldon";
 src: string = "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51PrW27sXWL.jpg";
 
-isDisabled: boolean = false;
 
-handleClick(){
-  alert('I am working!')
-};
+name2 : string = " box set";
+author2: string = "Dianna Gabaldon";
+src2: string = "https://images-na.ssl-images-amazon.com/images/I/51DE28Tk9qL._SY498_BO1,204,203,200_.jpg";
+
+// SETS A BUTTON TO FALSE
+// isDisabled: boolean = false;
+//  ALLOWS THE BUTTON TO BE DISABLED BY CLICKING ON IT
+// handleClick(){
+//   this.isDisabled = true;
+
+// };
+
+// TAKES INFO FROM THE INPUT BOX AND PRINTS IT IN THE P TAG
+//one way binding
+// myName:string='';
+
+// handleInput(  event: any){
+// this.myName = event.target.value;
+// };
+
+//two way binding
+// mystuffName: string = "";
+
+//ngIf
+isShowing = true;
+
+// if isShowing=true, then make it false and vice versa
+
+// toggleBooks(){
+//   this.isShowing =!this.isShowing;
+// }
+
+
+
 
   constructor() { }
 
